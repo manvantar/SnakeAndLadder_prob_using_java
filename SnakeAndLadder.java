fe040f32d99a04f190e13ea55415222d7cc1b9ec
manvantar <manukvshetty@gmail.com>
2021-03-25 00:09:16 +0530

    adding logic for postion 100

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -5,11 +5,13 @@ int start=0,end=100,count=0;
 Random rand1=new Random();
 int Dice=rand1.nextInt(6)+1;
 int Snake_or_ladder=rand1.nextInt(2);
-if(Snake_or_ladder==1 && start <=100){
+while(start!=100){
+if(Snake_or_ladder==1){
 start=start+Dice;
 }
 else if( start > Dice){
 start=start-1;
+}
 }
 }
 
