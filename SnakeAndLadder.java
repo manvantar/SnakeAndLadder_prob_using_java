91a0e7d9174859e8e899de1ed0a641f05b154973
manvantar <manukvshetty@gmail.com>
2021-03-25 00:13:23 +0530

    Report the number of

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -6,6 +6,7 @@ Random rand1=new Random();
 int Dice=rand1.nextInt(6)+1;
 int Snake_or_ladder=rand1.nextInt(2);
 while(start!=100){
+count++;
 if(Snake_or_ladder==1 && ((start+Dice) <=100)){
 start=start+Dice;
 }
@@ -13,6 +14,8 @@ else if( start > Dice){
 start=start-1;
 }
 }
+
+System.out.println(count+" times the dice is rolled");
 }
 
 
