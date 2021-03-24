308d25a33d51c54e62b0fe2c301438b67e116c89
manvantar <manukvshetty@gmail.com>
2021-03-25 00:05:16 +0530

    adding logic for snakeOrladder

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -4,7 +4,13 @@ public static void main(String[] args){
 int start=0,end=100,count=0;
 Random rand1=new Random();
 int Dice=rand1.nextInt(6)+1;
-
+int Snake_or_ladder=rand1.nextInt(2);
+if(Snake_or_ladder==1 && start <=100){
+start=start+Dice;
+}
+else if( start > Dice){
+start=start-1;
+}
 }
 
 
